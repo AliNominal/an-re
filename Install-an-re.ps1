8f90d30e29fefef6fa9d4ff18fc8808b3e922b5a
AliNominal <124453177+AliNominal@users.noreply.github.com>
2023-02-04 16:06:17 -0800

    Update Install-an-re.ps1

--- Install-an-re.ps1
+++ Install-an-re.ps1
@@ -1,2 +1,24 @@
 & ./setup/Install-Winget.ps1
 & ./setup/Install-OhMyPosh.ps1
+
+# Programming
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Microsoft.VisualStudio.2022.Community-Preview
+winget install -e --id Fork.Fork
+
+# techy shit
+winget install -e --id Microsoft.WindowsTerminal
+winget install -e --id AdoptOpenJDK.OpenJDK.16
+winget install -e --id Microsoft.Sysinternals.ProcessExplorer
+winget install -e --id voidtools.Everything
+winget install -e --id ShareX.ShareX
+winget install -e --id Microsoft.PowerToys
+
+# re
+winget install -e --id Hex-Rays.IDA.Free
+winget install -e --id Telerik.Fiddler.Classic
+
+# apps
+winget install -e --id Mozilla.Firefox
+winget install -e --id Discord.Discord
+winget install -e --id Valve.Steam
